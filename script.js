c66f76e53763a0a16c56ef093a4a4767ec9ee23f
Gregar <126393669+mrd010@users.noreply.github.com>
2023-09-24 15:38:13 +0330

    Add change input mechanism When user inputs wrong and not in range number it will correct itself

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const board = document.querySelector("#board");
 // functions
 
 function updateBoardSize() {
+  correctInput();
   const boardWidth = boardSizeInput.value;
   if (pixelCount != boardWidth ** 2) {
     while (pixelCount <= boardWidth ** 2) {
@@ -29,13 +30,34 @@ function updateBoardSize() {
   }
 }
 
-function InitializeBoard() {
-  updateBoardSize();
+// correct input by user if its value exceed 8-100 range and also round float number to int
+function correctInput() {
+  let inputValue = Math.floor(boardSizeInput.value);
+
+  if (inputValue > 100) {
+    boardSizeInput.value = 100;
+  } else if (inputValue < 8) {
+    boardSizeInput.value = 8;
+  }
 }
 
-function changeBoardSize() {
-  updateBoardSize();
-}
+// MAIN ----------------------------------------------------------------------------------------------------------
 
-InitializeBoard();
-changeBoardSizeBtn.addEventListener("click", changeBoardSize);
+// Initialize Board
+updateBoardSize();
+
+// update board size when clicking change board size button and change button back to normal
+changeBoardSizeBtn.addEventListener("click", () => {
+  updateBoardSize();
+  changeBoardSizeBtn.style.backgroundColor = "whitesmoke";
+});
+
+// in input change by user check and correct input and change button when input didn't apply
+boardSizeInput.addEventListener("change", () => {
+  correctInput();
+  if (pixelCount != boardSizeInput.value ** 2) {
+    changeBoardSizeBtn.style.backgroundColor = "lemonchiffon";
+  } else {
+    changeBoardSizeBtn.style.backgroundColor = "whitesmoke";
+  }
+});
