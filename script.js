1753a33e40950d1460182d5b43d282cbeb7ce21d
Gregar <126393669+mrd010@users.noreply.github.com>
2023-09-24 12:48:10 +0330

    Add first consts to Script

---
+++ script.js
@@ -0,0 +1,7 @@
+// Menu Consts
+const boardSizeInput = document.querySelector("#grid-width");
+const changeBoardSizeBtn = document.querySelector("change-button");
+const resetBoardBtn = document.querySelector("reset-button");
+
+// Board Consts
+const board = document.querySelector("#board");
